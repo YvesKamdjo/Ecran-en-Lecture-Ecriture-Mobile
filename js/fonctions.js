4269f2283dac76e8a7dbca93177868e8d964be3d
Hanae Mayer <hanae.mayer@gmail.com>
2013-03-19 14:13:31 +0100

    gettoken

--- js/fonctions.js
+++ js/fonctions.js
@@ -4,4 +4,24 @@ var refresh=false;
 function setIdentification(log, pass){
 	ecranEnLecture.login=log;
 	ecranEnLecture.password=pass;
+}
+
+function getDocumentReady(){
+	$(document).ready(function() {
+		getUrbaToken();
+	});
+}
+
+ function getUrbaToken(){
+ try{
+ $.ajax({
+		url : 'http://demo.urbaonline.com/pjeecran/authentication/getToken?login='+ecranEnLecture.login+'&password='+ecranEnLecture.password,
+		dataType : 'jsonp',
+		jsonpCallback: 'setValidToken',			
+	})
 	}
+	catch(e){
+	console.log(e);
+
+	}	
+}
